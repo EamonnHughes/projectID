2b65dc5a0a5fa4cb8e1e930329879f84db6396c1
arathorn2 <43751985+arathorn2@users.noreply.github.com>
2021-07-06 09:14:53 -0400

    rotation on y axis fixed

--- IDproj/Assets/scripts/facemouse.cs
+++ IDproj/Assets/scripts/facemouse.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class facemouse : MonoBehaviour
-{
-    public GameObject player;
-    
-  
+{  
 
     // Update is called once per frame
     void Update()
     {
-   
+ Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+ Plane p = new Plane( Vector3.up, transform.position );
+ if( p.Raycast( mouseRay, out float hitDist) ){
+     Vector3 hitPoint = mouseRay.GetPoint( hitDist );
+     transform.LookAt( hitPoint );
+ }
     }
 }
