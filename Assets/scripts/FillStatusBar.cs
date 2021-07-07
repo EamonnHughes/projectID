7033ed446944265d6865c601eb52a0531ea38981
Enda111 <30953879+Enda111@users.noreply.github.com>
2021-07-06 21:29:19 -0400

    health bar

--- Assets/scripts/FillStatusBar.cs
+++ Assets/scripts/FillStatusBar.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HealthBar : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
     public playerHealth playerHealth;
     public Image fillImage;
