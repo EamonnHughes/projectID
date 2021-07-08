89bfa44a25712a7d3cd8311e57287a17c2c6862c
arathorn2 <43751985+arathorn2@users.noreply.github.com>
2021-07-08 18:30:58 -0400

    fixed things

--- Assets/scripts/PlayerMove.cs
+++ Assets/scripts/PlayerMove.cs
@@ -7,14 +7,20 @@ public class PlayerMove : MonoBehaviour
     Rigidbody body;
 
     float horizontal;
+
     float vertical;
+
     public float dashSpeed;
 
     public float runSpeed = 20.0f;
+
     public float dodgeSpeed;
+
     public AudioSource walkSound;
-    Vector3 leftr = new Vector3(0, -50, 0);
-    Vector3 rightr = new Vector3(0, 50, 0);
+
+    Vector3 leftr = new Vector3(0, -80, 0);
+
+    Vector3 rightr = new Vector3(0, 80, 0);
 
     void Start()
     {
@@ -23,25 +29,32 @@ public class PlayerMove : MonoBehaviour
 
     void Update()
     {
-        Vector3 rotation = new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + 180, transform.rotation.eulerAngles.z);
+        Vector3 rotation =
+            new Vector3(transform.rotation.eulerAngles.x,
+                transform.rotation.eulerAngles.y + 180,
+                transform.rotation.eulerAngles.z);
         if (Input.GetKey(KeyCode.W))
         {
             transform.Translate((Vector3.forward * 4) * Time.deltaTime);
             playSoundEffect();
-            
         }
-        //dash
-        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift) && StaminaBar.instance.currentStamina >= 1)
-        {
-            body.AddForce(transform.forward * dashSpeed * Time.deltaTime, ForceMode.Impulse);
-            StaminaBar.instance.UseStamina(1);
 
+        //dash
+        if (
+            Input.GetKey(KeyCode.W) &&
+            Input.GetKey(KeyCode.LeftShift) &&
+            StaminaBar.instance.currentStamina >= 1
+        )
+        {
+            body
+                .AddForce(transform.forward * dashSpeed * Time.deltaTime,
+                ForceMode.Impulse);
+            StaminaBar.instance.UseStamina(1);
         }
         if (Input.GetKey(KeyCode.S))
         {
             transform.Translate((Vector3.back * 4) * Time.deltaTime);
             playSoundEffect();
-    
         }
         if (Input.GetKey(KeyCode.A))
         {
@@ -64,25 +77,43 @@ public class PlayerMove : MonoBehaviour
         //    body.AddForce(Vector3.right * dashSpeed * Time.deltaTime, ForceMode.Impulse);
         //   StaminaBar.instance.UseStamina(1);
         //}
-        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.LeftShift) && StaminaBar.instance.currentStamina >= 1)
+        if (
+            Input.GetKey(KeyCode.A) &&
+            Input.GetKey(KeyCode.LeftShift) &&
+            StaminaBar.instance.currentStamina >= 1
+        )
         {
-            body.AddForce(Vector3.left * dashSpeed * Time.deltaTime, ForceMode.Impulse);
+            body
+                .AddForce(Vector3.left * dashSpeed * Time.deltaTime,
+                ForceMode.Impulse);
             StaminaBar.instance.UseStamina(1);
         }
-        if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.LeftShift) && StaminaBar.instance.currentStamina >= 1)
+        if (
+            Input.GetKey(KeyCode.D) &&
+            Input.GetKey(KeyCode.LeftShift) &&
+            StaminaBar.instance.currentStamina >= 1
+        )
         {
-            body.AddForce(Vector3.right * dashSpeed * Time.deltaTime, ForceMode.Impulse);
+            body
+                .AddForce(Vector3.right * dashSpeed * Time.deltaTime,
+                ForceMode.Impulse);
             StaminaBar.instance.UseStamina(1);
         }
-        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.LeftShift) && StaminaBar.instance.currentStamina >= 1)
+        if (
+            Input.GetKey(KeyCode.S) &&
+            Input.GetKey(KeyCode.LeftShift) &&
+            StaminaBar.instance.currentStamina >= 1
+        )
         {
-            body.AddForce(Vector3.back * dashSpeed * Time.deltaTime, ForceMode.Impulse);
+            body
+                .AddForce(Vector3.back * dashSpeed * Time.deltaTime,
+                ForceMode.Impulse);
             StaminaBar.instance.UseStamina(1);
         }
     }
+
     public void playSoundEffect()
     {
         walkSound.Play();
     }
-
 }
--- Assets/scripts/hitenemy.cs
+++ Assets/scripts/hitenemy.cs
@@ -7,12 +7,19 @@ public class hitenemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "enemy")
+        {
+            Destroy(collision.gameObject);
+            Destroy(this.gameObject);
+        }
     }
 }
